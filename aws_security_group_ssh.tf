681dff59dc290d27233e12dd6002aa0d3ff50135
Sion Smith <sion.smith@gmail.com>
2019-01-09 07:49:06 +0000

    testing without VPC config

--- aws_security_group_ssh.tf
+++ aws_security_group_ssh.tf
@@ -1,24 +1,24 @@
-resource "aws_security_group" "ssh" {
-  name        = "${var.environment}-33N-CODEBUILD-SG"
-  description = "Managed by Terraform"
-  vpc_id      = "${data.aws_vpc.codebuild.id}"
-
-  # from bastion
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "TCP"
-    self = true
-    security_groups = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = "${merge(var.common_tags,
-    map("Name", "${var.environment}-33N-CODEBUILD-SG"))}"
-}
+//resource "aws_security_group" "ssh" {
+//  name        = "${var.environment}-33N-CODEBUILD-SG"
+//  description = "Managed by Terraform"
+//  vpc_id      = "${data.aws_vpc.codebuild.id}"
+//
+//  # from bastion
+//  ingress {
+//    from_port       = 22
+//    to_port         = 22
+//    protocol        = "TCP"
+//    self = true
+//    security_groups = ["0.0.0.0/0"]
+//  }
+//
+//  egress {
+//    from_port   = 0
+//    to_port     = 0
+//    protocol    = "-1"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//
+//  tags = "${merge(var.common_tags,
+//    map("Name", "${var.environment}-33N-CODEBUILD-SG"))}"
+//}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "build_timeout" { default = "60" }
-variable "cd_buildspect" {}
 variable "environment" {}
 variable "vpc_cidr" {}
 variable "vpc_id" {}
