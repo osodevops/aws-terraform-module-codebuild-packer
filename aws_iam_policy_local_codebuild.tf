f44f0df797b47f9fdc32c3bd48b621d924fd98ab
Sion Smith <sion.smith@gmail.com>
2019-01-11 06:38:35 +0000

    updated to Role descriptions

--- aws_iam_policy_local_codebuild.tf
+++ aws_iam_policy_local_codebuild.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_policy" "local_codebuild" {
   name        = "CodebuildPolicy"
-  description = "Policy used in trust relationship with CodeBuild"
+  description = "Managed by Terraform: Policy used in trust relationship with CodeBuild"
 
   policy = "${data.aws_iam_policy_document.local_codebuild.json}"
 }
--- aws_iam_role.tf
+++ aws_iam_role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "local_codebuild_role" {
   name = "Codebuild-Local-Role"
-
+  description = "Managed by Terraform"
   assume_role_policy = "${data.aws_iam_policy_document.forrole.json}"
 }
