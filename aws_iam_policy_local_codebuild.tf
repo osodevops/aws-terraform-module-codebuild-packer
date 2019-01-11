d9b261b15b27eaa17e209a483b808bf50e760bb0
Sion Smith <sion.smith@gmail.com>
2019-01-11 16:29:03 +0000

    more policy updates

--- aws_iam_policy_local_codebuild.tf
+++ aws_iam_policy_local_codebuild.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "local_codebuild" {
-  name        = "CodebuildPolicy"
+  name        = "CODEBUILD-${upper(var.project_name)}}-POLICY"
   description = "Managed by Terraform: Policy used in trust relationship with CodeBuild"
   path = "/service-role/"
   policy = "${data.aws_iam_policy_document.local_codebuild.json}"
