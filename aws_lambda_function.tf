b2ea9a4f2c6d5c59a1160bfd169a68530c8d1b71
Sion Smith <sion.smith@gmail.com>
2019-08-07 20:57:40 +0100

    updated as per Rich's changes.

--- aws_lambda_function.tf
+++ aws_lambda_function.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "ami_encryption_lambda" {
   count         = "${var.encrypt_ami}"
   filename      = "${data.archive_file.ami_encryption.0.output_path}"
   description   = "Responsible for creating AMI with encrypted root volume."
-  function_name = "${var.environment}-AMI-ENCRYPTION-FUNCTION"
+  function_name = "${upper(var.environment)}-AMI-ENCRYPTION-FUNCTION"
 
   role          = "${aws_iam_role.ami_encrypt_lambda.arn}"
   handler       = "ami_encryption.lambda_handler"
--- aws_lambda_iam_policy_document.tf
+++ aws_lambda_iam_policy_document.tf
@@ -14,21 +14,25 @@ data "aws_iam_policy_document" "lambda_config_trust" {
 data "aws_iam_policy_document" "lambda_config_policy" {
   count         = "${var.encrypt_ami}"
   statement {
+    effect = "Allow"
     actions   = ["ec2:Describe*", "ec2:CreateSnapshot", "ec2:CreateImage", "ec2:CreateTags*", "ec2:CopyImage"]
     resources = ["*"]
   }
 
   statement {
+    effect = "Allow"
     actions   = ["cloudwatch:*"]
     resources = ["*"]
   }
 
   statement {
+    effect = "Allow"
     actions   = ["logs:*"]
     resources = ["arn:aws:logs:*:*:*"]
   }
 
   statement {
+    effect = "Allow"
     actions = ["kms:*"]
     resources = ["*"]
   }
