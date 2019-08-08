19c8bc51aadb4b7fc8606d9cdacf59bc8d582649
Sion Smith <sion.smith@gmail.com>
2019-08-08 11:35:50 +0100

    added missing cloudwatch rule.

---
+++ aws_cloudwatch_event_rule_build_success.tf
@@ -0,0 +1,29 @@
+resource "aws_cloudwatch_event_rule" "build_alert" {
+  # Create cloudwatch event that monitors build events and passes them to ami encryption lambda function
+  name = "${upper(var.project_name)}-CODEBUILD-SUCESS-ALERT"
+  description = "Send alerts to encrypt AMI on build success."
+
+  event_pattern = <<PATTERN
+        {
+          "source": [
+            "com.ami.builder"
+          ],
+          "detail-type": [
+            "AmiBuilder"
+          ],
+          "detail": {
+            "AmiStatus": [
+              "Created"
+            ]
+          }
+        }
+      PATTERN
+}
+
+resource "aws_cloudwatch_event_target" "lamba_alert" {
+  # Create ami encryption target
+  rule      = "${aws_cloudwatch_event_rule.build_alert.name}"
+  target_id = "ami-encryption-lambda"
+  arn       = "${aws_lambda_function.ami_encryption_lambda.arn}"
+}
+
