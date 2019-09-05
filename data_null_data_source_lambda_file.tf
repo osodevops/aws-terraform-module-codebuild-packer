b6e789190e2697ce1ac71fce425ce47ed4944db2
Sion Smith <sion.smith@gmail.com>
2019-09-05 11:23:31 +0100

    removed lambda files from codebuild project

--- data_null_data_source_lambda_file.tf
+++
@@ -1,6 +0,0 @@
-data "null_data_source" "lambda_file" {
-  count         = "${var.encrypt_ami}"
-  inputs {
-    filename = "${substr("${path.module}/functions/ami_encryption.py", length(path.cwd) + 1, -1)}"
-  }
-}
