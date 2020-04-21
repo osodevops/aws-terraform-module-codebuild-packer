07fcd34b614862eb361ff2f37dcce03e76bc8503
Richard Gutkowski <richard@ososolution.com>
2020-04-21 11:23:48 +0100

    Removed the lambda and all associated files from the codebuild tf module. This will now be controlled from an another module.

--- aws_lambda_iam_role.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_iam_role" "ami_encrypt_lambda" {
-  count              = var.encrypt_ami ? 1 : 0
-  name               = "${var.environment}-AMI-ENCRYPTION-LAMBDA-ROLE"
-  description        = "Allows Lambda function to execute AMI copy with encrypted root volume."
-  assume_role_policy = data.aws_iam_policy_document.lambda_config_trust[0].json
-}
-
